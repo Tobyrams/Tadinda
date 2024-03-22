ac74d96783367f98aad99b770c6f8847675c52e2
Tobyrams@gmail.com <Tobyrams@gmail.com>
2024-03-22 23:41:29 +0200

    Created website navigation for all screen sizes

---
+++ script.js
@@ -0,0 +1,7 @@
+// When the hamburger menu is click on the appropriate screen size
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
