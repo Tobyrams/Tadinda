e181288d742ebe7aa40e41bec021b79c98dd9cde
Tobyrams@gmail.com <Tobyrams@gmail.com>
2024-03-24 21:55:59 +0200

    Made Menu page content

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function toggleMenu() {
 function openInNewTab(url) {
   window.open(url, "_blank");
 }
+
+// Menu Heading arrow annimation
+document.addEventListener("DOMContentLoaded", () => {
+  const menuHeading = document.querySelector(".Menu-Arrow");
+  menuHeading.classList.add("bounce-infinate");
+  document.body.appendChild(bounce);
+});
+// Home page Hero arrow animation 5 times
+document.addEventListener("DOMContentLoaded", () => {
+  const heroArrow = document.querySelector(".arrow-down-icon");
+  heroArrow.classList.add("bounce-5");
+  document.body.appendChild(bounce);
+});
