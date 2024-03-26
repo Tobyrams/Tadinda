7209d56f8cd44658c17545042e86fec73d772b30
Tobyrams@gmail.com <Tobyrams@gmail.com>
2024-03-26 19:33:17 +0200

    Added Scroll to top button.
    Added scroll to top button on all pages and screen sizes.

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
   heroArrow.classList.add("bounce-5");
   document.body.appendChild(bounce);
 });
+
+// Scroll to top button
+
+//Get the button
+var mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 700 ||
+    document.documentElement.scrollTop > 700
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
