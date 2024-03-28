41e4de8beb6c12dbfd8bd53cf4a237109c214448
Tobyrams@gmail.com <Tobyrams@gmail.com>
2024-03-28 23:01:46 +0200

    Added Customer reviews section
    Added customers review section to the home page.

--- script.js
+++ script.js
@@ -50,3 +50,30 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// SwiperJS
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 50,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
